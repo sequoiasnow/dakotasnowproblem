7254d5199c30b1b364cdbc5f70f34533eb16f953
Sequoia Snow <the1codemaster@gmail.com>
2015-10-04 17:06:17 -0700

    Fixed gulpfile completly l

--- dist/js/plugins.js
+++ dist/js/plugins.js
@@ -1,3 +1,28 @@
+jQuery(document).ready(function($) {
+    /**
+     * Functions that is fired when a resize event is complete. This is used
+     * primarily for the window element.
+     *
+     * Example use `$(window).endResize()`
+     */
+
+    $.fn.endResize = function( callable func ) {
+
+        var resizeID;
+
+        var donResizing = function() {
+            func.call( this );
+        }
+
+        this.resize( function() {
+            clearTimeout( resizeID );
+
+            resizeId = setTimeout( donResizing, 500 );
+        } );
+
+    }
+});
+
 jQuery( document ).ready(function($) {
     /**
      * Creates a grid system based using absolutly positioned eleemnts. All
--- gulpfile.js
+++ gulpfile.js
@@ -17,26 +17,26 @@ gulp.task( 'styles', function() {
 
 // Automate the admin react compilation into a single file.
 gulp.task( 'js', function() {
-    gulp.src( 'src/plugins/*.js' )
+    gulp.src( 'src/js/plugins/*.js' )
         .pipe(concat( 'plugins.js' ))
         .pipe(gulp.dest( 'dist/js/' ));
 
-    gulp.src( 'src/elements/*.js' )
+    gulp.src( 'src/js/elements/*.js' )
         .pipe(concat( 'elements.js' ))
         .pipe(gulp.dest( 'dist/js/' ));
 
-    gulp.src( 'src/pages/*.js' )
+    gulp.src( 'src/js/pages/*.js' )
         .pipe(concat( 'pages.js' ))
         .pipe(gulp.dest( 'dist/js/' ));
 
-    gulp.src( 'src/libraries/*.js' )
+    gulp.src( 'src/js/libraries/*.js' )
         .pipe(concat( 'pages.js' ))
         .pipe(gulp.dest( 'dist/js/' ));
 });
 
 gulp.task( 'watch', function() {
     gulp.watch( 'src/sass/**/*.scss', [ 'styles' ] );
-    gulp.watch( '/src/js/**/*.js', [ 'js' ] )
+    gulp.watch( 'src/js/**/*.js', [ 'js' ] )
 });
 
 gulp.task( 'default', [ 'styles', 'js' ] );
